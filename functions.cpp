21159357a3db1f0271761f3889f9c1adf0f44212
Lifeformwp <vielon.indie@yandex.ru>
2016-05-09 01:09:13 +0300

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -1,23 +1,60 @@
 #include "functions.h"
 
-	void BlackWhiteImg::setData(int InSize, unsigned char *DataOut, unsigned char *DataIn)
+	void ImgWork::setData(vector<Mat>::iterator img, vector<Mat>::iterator img3, int NumThreadX)
 	{
-		SizeInClass = InSize;
-		DataInClass = DataIn;
-		DataOutClass = DataOut;
+		SizeINImg = (*img).step * (*img).rows;
+		SizeOutImg = (*img3).step * (*img3).rows;
+		DataImg = (*img).data;
+		DataImg2 = (*img3).data;
+		BlocksNumber = ((*img).cols * (*img).rows)/NumThreadX;
+	}
+
+	void ImgWork::setDataSingle(Mat img, Mat img3, int NumThreadX)
+	{
+		SizeINImg = img.step * img.rows;
+		SizeOutImg = img3.step * img3.rows;
+		DataImg = img.data;
+		DataImg2 = img3.data;
+		BlocksNumber = (img.cols * img.rows)/NumThreadX;
+	}
+
+	int ImgWork::getSizeBlocks()
+	{
+		return SizeINImg, BlocksNumber, SizeOutImg;
+	}
+
+	unsigned char ImgWork::getData12()
+	{
+		return *DataImg, *DataImg2;
 	}
 
 
-	void BlackWhiteImg::Grayscale()
+	void Grayscale::GSLightness()
 	{
-		for(int i = 0,  j = 0; i < SizeInClass; i += 3, j++)
+		for(int i = 0,  j = 0; i < SizeINImg; i += 3, j++)
 		{
-			DataOutClass[j] = (DataInClass[i] + DataInClass[i + 1] + DataInClass[i + 2]) / 3;
+			int min= DataImg[i]<DataImg[i + 1]?DataImg[i]:DataImg[i + 1];
+			min= DataImg[i + 2]<min?DataImg[i + 2]:min;
+			int max = DataImg[i]<DataImg[i + 1]?DataImg[i]:DataImg[i + 1];
+			max=DataImg[i + 2]<max?DataImg[i + 2]:max;
+			DataImg2[j] = (max + min)/2;
 		}
-
 	}
 
-	unsigned char BlackWhiteImg::getGrayscaleData()
+	void Grayscale::GSLuminosity()
 	{
-		return *DataOutClass;
+		for(int i = 0,  j = 0; i < SizeINImg; i += 3, j++)
+		{
+			DataImg2[j] = ((0.21 * DataImg[i]) + (0.72 * DataImg[i + 1]) + (0.07 * DataImg[i + 2]));
+		}
 	}
+
+	void Grayscale::GSAverage()
+	{
+		for(int i = 0,  j = 0; i < SizeINImg; i += 3, j++)
+		{
+
+			DataImg2[j] = (DataImg[i] + DataImg[i + 1] + DataImg[i + 2])/3;
+		}
+	}
+
