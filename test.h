7e1d531d2e3fb360e1ac8f6f6ed7b8c5b34bf51d
Lifeformwp <vielon.indie@yandex.ru>
2016-05-09 01:10:09 +0300

    Update test.h

--- test.h
+++ test.h
@@ -1,15 +1,8 @@
 struct params
 {
-	unsigned char *DatIn;
-	unsigned char *DatOut;
-	//unsigned int SizeOut;
-	int SizeIn;
-	int NumBlocksX;
-	int NumThreadsX;
-	int SizeOut;
-
-	unsigned char  *devDatIn;
+	unsigned char *chard;
+	unsigned char *devDatIn;
 	unsigned char *devDatOut;
 };
 
-extern "C" int my_cuda1(unsigned char *devDatIn, unsigned char *devDatOut, int NumBlocksX, int NumThreadsX);
+extern "C" int my_cuda1(unsigned char *devDatIn, unsigned char *devDatOut, int NumBlocksX, int NumThreadsX, unsigned char* chard);
