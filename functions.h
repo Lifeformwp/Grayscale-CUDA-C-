244b1ae8024a0df7a750109066692c98d944c1bd
Lifeformwp <vielon.indie@yandex.ru>
2016-05-09 01:08:49 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,18 +1,44 @@
 #pragma once
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/core.hpp>
+#include <vector>
+using namespace cv;
+using namespace std;
 
-class BlackWhiteImg
+class ImgWork
 {
+public:
+	int SizeINImg, SizeINImg2;
+	int BlocksNumber, BlocksNumber2;
+	int SizeOutImg, SizeOutImg2;
+	unsigned char* DataImg;
+	unsigned char* DataImg2;
+	unsigned char* DataImg1;
+	unsigned char* DataImg22;	
 
+	void setData(vector<Mat>::iterator img, vector<Mat>::iterator img3, int NumThreadX);
+	void setDataSingle(Mat img, Mat img3, int NumThreadX);
+
+	int getSizeBlocks();
+
+	unsigned char getData12();
+	
+private:
+
+};
+
+class Grayscale : public ImgWork
+{
 public:
 
-	void setData(int InSize, unsigned char *DataOut, unsigned char *DataIn);
+	void GSLightness();
 
+	void GSLuminosity();
 
-	void Grayscale();
+	void GSAverage();
 
-	unsigned char getGrayscaleData();
-
-private:
-	unsigned char *DataInClass, *DataOutClass;
-	int SizeInClass;
 };
+
+
+
